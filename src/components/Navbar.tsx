2bf8879c3426fc3213bc0167707ea86e27652430
rohhitsingh19 <154086284+rohhitsingh19@users.noreply.github.com>
2025-05-04 00:50:15 +0530

    new commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,10 +19,10 @@ function Navbar({ className }: { className?: string }) {
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/courses">All Courses</HoveredLink>
-            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/courses">Advance Composition</HoveredLink>
-            <HoveredLink href="/courses">Songwriting</HoveredLink>
-            <HoveredLink href="/courses">Music Production</HoveredLink>
+            <HoveredLink href="/courses">DSA Fundamentals</HoveredLink>
+            <HoveredLink href="/courses">Web Development Bootcamp</HoveredLink>
+            <HoveredLink href="/courses">DevOps Mastery</HoveredLink>
+            <HoveredLink href="/courses">Machine Learning Engineering</HoveredLink>
           </div>
         </MenuItem>
         < Link href={"/contact"}>
